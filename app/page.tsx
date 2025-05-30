fdd95612f2b081d7d3fcf7f294fa9e41cbabcffc
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2025-05-30 04:07:16 +0100

    Add key to iconButtons

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         showSearchBar={false}
         buttonData={{ text: "Add Session", icon: MdAdd }}
         leftSection={<AppLogo />}
-        iconButtons={[<ModeToggle />]}
+        iconButtons={[<ModeToggle key="mode-toggle"/>]}
       />
     </div>
   );
